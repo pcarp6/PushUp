e0f83b6e34d60b75bffe56ebea6a513f39283607
pcarp <qianchrischeng@gmail.com>
2020-03-13 11:04:16 -0400

    Initial commit

---
+++ app/src/main/java/android/example/pushup/MainActivity.java
@@ -0,0 +1,17 @@
+package android.example.pushup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void TestUpload(){
+        int i = 1+1;
+    }
+}
